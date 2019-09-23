c28fb6d562bcce7df5298d585c47381d0c4320c3
Nick Prokesch <nick@prokes.ch>
2019-09-23 00:46:11 -0500

    Add onHashChange callback prop to report all changes to the hash
    Signed-off-by: Nick Prokesch <nick@prokes.ch>

--- src/ScrollableAnchor.js
+++ src/ScrollableAnchor.js
@@ -1,33 +1,35 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
-import Manager from './Manager'
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import Manager from './Manager';
 
 export default class ScrollableAnchor extends Component {
   static propTypes = {
     children: PropTypes.node,
     id: PropTypes.string,
-  }
+    onHashChange: PropTypes.func,
+  };
 
   constructor(props) {
-    super(props)
-    this.id = props.id || props.children.ref
+    super(props);
+    this.id = props.id || props.children.ref;
+    !!props.onHashChange && Manager.addCallback(props.onHashChange);
   }
 
   componentDidMount() {
-    const element = ReactDOM.findDOMNode(this.refs[Object.keys(this.refs)[0]])
-    Manager.addAnchor(this.id, element)
+    const element = ReactDOM.findDOMNode(this.refs[Object.keys(this.refs)[0]]);
+    Manager.addAnchor(this.id, element);
   }
 
   componentWillUnmount() {
-    Manager.removeAnchor(this.id)
+    Manager.removeAnchor(this.id);
   }
 
   render() {
-    const {children, id} = this.props
+    const {children, id} = this.props;
 
     return React.cloneElement(children, {
       ref: children.ref || id,
-    })
+    });
   }
 }
